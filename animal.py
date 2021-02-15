84e2198754c7653a35aba9f6c2cf452fe2b55e61
Mrozolga <m.olgaa97@gmail.com>
2021-02-15 19:51:54 +0100

    go back to first master (#5)
    Co-authored-by: Anna <anna@github.training.com>

--- animal.py
+++ animal.py
@@ -20,24 +20,7 @@ class Dog(Animal):
         print("Woof!")
 
 
-class Cat(Animal):
-    def __init__(self, name, age):
-        super().__init__(name, age)
-        self.type = "cat"
-
-    def speak(self):
-        super().speak()
-        print("Meow!")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        raise Exception(f"Not enough arguments or {sys.argv[3]} different than 'dog' or 'cat'!")
-    if sys.argv[3] not in ['cat', 'dog']:
-        raise Exception(f"{sys.argv[3]} different than 'dog' or 'cat'!")
-    if sys.argv[3] == 'cat':
-        called_animal = Cat(sys.argv[1], sys.argv[2])
-    elif sys.argv[3] == 'dog':
-        called_animal = Dog(sys.argv[1], sys.argv[2])
+    called_animal = Dog(sys.argv[1], sys.argv[2])
     called_animal.speak()
 
