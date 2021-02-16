575396d74ba1ef5ee4116fc1b69f9d4f473f96b3
Mrozolga <m.olgaa97@gmail.com>
2021-02-16 16:02:25 +0100

    Cat feature2 (#10)
    * implement Cat feature
    
    * execution of Animal class
    
    Co-authored-by: Anna <anna@training.com>
    Co-authored-by: Tom <tom@training.com>

--- animal.py
+++ animal.py
@@ -20,6 +20,19 @@ class Dog(Animal):
         print("Woof!")
 
 
+class Cat(Animal):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+        self.type = "cat"
+
+    def speak(self):
+        super().speak()
+        print("Meow!")
+
+
 if __name__ == "__main__":
-    called_animal = Dog(sys.argv[1], sys.argv[2])
+    if sys.argv[1] == 'dog':
+        called_animal = Dog(sys.argv[1], sys.argv[2])
+    if sys.argv[1] == 'cat':
+        called_animal = Cat(sys.argv[1], sys.argv[2])
     called_animal.speak()
